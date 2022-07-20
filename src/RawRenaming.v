548e0df8dc409f554b29a9ffd9eb50341586e8c9
Leo White <leo@lpw25.net>
2022-07-20 09:25:33 +0100

    WIP

--- src/RawRenaming.v
+++ src/RawRenaming.v
@@ -1,59 +1,164 @@
 Require Import Label PeanoNat Psatz Ring Compare_dec StrictProp.
 Require Import Var.
 
-Inductive raw_renaming :=
-  | raw_renaming_id : raw_renaming
-  | raw_renaming_extend :
-      var -> raw_renaming -> var -> raw_renaming.
+Definition incoming_item : Set := (option label) * nat.
 
-Fixpoint apply_raw_renaming_var r : var_op 0 0 :=
-  match r with
-  | raw_renaming_id => var_op_id
-  | raw_renaming_extend vl r vr =>
-    pop_var vl @ lift_var_op (apply_raw_renaming_var r) @ push_var vr
+Definition incoming := list incoming_item.
+
+Definition apply_incoming_item
+           (vn : var + nat) (inc : incoming_item)
+  : var + nat :=
+  match vn with
+  | inl v =>
+    match inc with
+    | (l, m) =>
+      if label_opt_eqb l (v_label_opt v) then
+        match v_nat v with
+        | 0 => inr m
+        | S n => inl (mk_var (v_label_opt v) n)
+        end
+      else
+        vn
+    end
+  | inr n => vn
   end.
 
-Set Primitive Projections.
-Record renaming_rhs :=
-  mk_renaming_rhs {
-      rhs_raw_renaming : raw_renaming;
-      rhs_push : var
-    }.
-Add Printing Constructor renaming_rhs.
-Unset Primitive Projections.
+Definition apply_incoming inc vn :=
+  List.fold_left apply_incoming_item inc vn.
 
-Definition renaming_rhs_extend vl r vr :=
-  mk_renaming_rhs
-    (raw_renaming_extend vl
-        (rhs_raw_renaming r)
-        (unshift_var (rhs_push r) vr))
-    (shift_var vr (rhs_push r)).
+Lemma apply_incoming_cons_inv hd tl vn :
+  apply_incoming (cons hd tl) vn
+  = apply_incoming tl (apply_incoming_item vn hd).
+Proof. reflexivity. Qed.
 
-Fixpoint transpose_push_raw_renaming v r {struct r} :=
-  match r with
-  | raw_renaming_id => mk_renaming_rhs raw_renaming_id v
-  | raw_renaming_extend vl r vr =>
-    if var_eqb v vl then mk_renaming_rhs r vr
-    else renaming_rhs_extend
-           (unshift_var v vl)
-           (transpose_push_raw_renaming (unshift_var vl v) r)
-           vr
+Definition incoming_item_less_than
+           (n : nat) (inc : incoming_item) :=
+  Nat.ltb (snd inc) n.
+
+Definition incoming_less_than (n : nat) (inc : incoming) :=
+  List.forallb (incoming_item_less_than n) inc.
+
+Lemma incoming_less_than_cons_inv n hd tl :
+  incoming_less_than n (cons hd tl)
+  = andb (incoming_item_less_than n hd)
+         (incoming_less_than n tl).
+Proof. reflexivity. Qed.
+
+Definition incoming_item_is_less_than n inc : SProp :=
+  if incoming_item_less_than n inc then sUnit
+  else sEmpty.
+
+Definition incoming_is_less_than n inc : SProp :=
+  if incoming_less_than n inc then sUnit
+  else sEmpty.
+
+Lemma incoming_is_less_than_head n inc rest :
+  incoming_is_less_than n (inc :: rest)%list ->
+  incoming_item_is_less_than n inc.
+Proof.
+  unfold incoming_is_less_than, incoming_item_is_less_than.
+  rewrite incoming_less_than_cons_inv.
+  destruct incoming_item_less_than; easy.
+Qed.
+
+Lemma incoming_is_less_than_tail n inc rest :
+  incoming_is_less_than n (inc :: rest)%list ->
+  incoming_is_less_than n rest.
+Proof.
+  unfold incoming_is_less_than, incoming_item_is_less_than.
+  rewrite incoming_less_than_cons_inv.
+  destruct incoming_item_less_than; easy.
+Qed.
+
+Definition outgoing_item := option label.
+
+Definition outgoing := list outgoing_item.
+
+Definition apply_outgoing_item
+           (vn : var + nat) (otg : option label)
+  : var + nat :=
+  match vn with
+  | inr n =>
+    match n with
+    | 0 => inl (mk_var otg 0)
+    | S n => inr n
+    end
+  | inl v =>
+    if label_opt_eqb otg (v_label_opt v) then
+      inl (mk_var (v_label_opt v) (S (v_nat v)))
+    else
+      vn
   end.
 
-Fixpoint compose_raw_renaming r1 r2 :=
-  match r1 with
-  | raw_renaming_id => r2
-  | raw_renaming_extend vl r1 vr =>
-    let rhs := transpose_push_raw_renaming vr r2 in
-    raw_renaming_extend
-      vl
-      (compose_raw_renaming r1 (rhs_raw_renaming rhs))
-      (rhs_push rhs)
+Definition apply_outgoing otg vn :=
+  List.fold_left apply_outgoing_item otg vn.
+
+Lemma apply_outgoing_cons_inv hd tl vn :
+  apply_outgoing (cons hd tl) vn
+  = apply_outgoing tl (apply_outgoing_item vn hd).
+Proof. reflexivity. Qed.
+
+Definition vn_less_than n (vn : var + nat) :=
+  match vn with
+  | inl _ => true
+  | inr m => Nat.ltb m n
   end.
 
-Fixpoint inverse_raw_renaming r :=
-  match r with
-  | raw_renaming_id => r
-  | raw_renaming_extend vl r vr =>
-      raw_renaming_extend vr (inverse_raw_renaming r) vl
-  end.
+Definition vn_is_less_than n vn : SProp :=
+  if vn_less_than n vn then sUnit
+  else sEmpty.
+
+Lemma vn_is_less_than_inr_succ n m :
+  vn_is_less_than (S n) (inr (S m)) ->
+  vn_is_less_than n (inr m).
+Proof. induction n; easy. Qed.
+
+Lemma apply_incoming_less_than n inc vn :
+  incoming_is_less_than n inc ->
+  vn_is_less_than n vn ->
+  vn_is_less_than n (apply_incoming inc vn).
+Proof.
+  intro Hinc.
+  generalize dependent vn.
+  induction inc as [|[l m] tl]; intros vn Hv; try easy.
+  apply incoming_is_less_than_head in Hinc as Hinc1.
+  apply incoming_is_less_than_tail in Hinc as Hinc2.
+  rewrite apply_incoming_cons_inv.
+  apply (IHtl Hinc2).
+  destruct vn as [v|o]; cbn; try easy.
+  destruct label_opt_eqb, (v_nat v); easy.
+Qed.
+
+Lemma apply_outgoing_less_than n otg vn :
+  vn_is_less_than (n + List.length otg) vn ->
+  vn_is_less_than n (apply_outgoing otg vn).
+Proof.
+  generalize dependent vn.
+  induction otg as [|l tl]; intros vn Hv.
+  - rewrite <- plus_n_O in Hv; easy.
+  - rewrite apply_outgoing_cons_inv.
+    apply IHtl.
+    destruct vn as [v|[|m]]; cbn; try easy.
+    + destruct label_opt_eqb; easy.
+    + cbn in Hv.
+      rewrite <- plus_n_Sm in Hv.
+      apply vn_is_less_than_inr_succ; easy.
+Qed.
+
+Lemma apply_renaming_var_less_than v inc otg :
+  incoming_is_less_than (List.length otg) inc ->
+  vn_is_less_than 0
+    (apply_outgoing otg (apply_incoming inc (inl v))).
+Proof.
+  intros Hin.
+  apply apply_outgoing_less_than.
+  apply apply_incoming_less_than; easy.
+Qed.
+
+Definition apply_renaming inc otg
+           {H : incoming_is_less_than (List.length otg) inc}
+           v :=
+  match apply_outgoing otg (apply_incoming inc (inl v)) with
+  | inl v' => v'
+  | inr _ => 
+
--- src/Var.v
+++ src/Var.v
@@ -1,118 +1,56 @@
 Require Import Label String PeanoNat Compare_dec
         StrictProp Setoid Morphisms.
 
-(* Name indices are [nat]s *)
-
-Definition index := nat.
-
-Definition index_dec := Nat.eq_dec.
-
-(* Free names are a pair of a string and an index *)
+(* Variables are a pair of an optional label and a nat *)
 
 Set Primitive Projections.
-Record name := mkname { n_label : label; n_index : index }.
-Add Printing Constructor name.
+Record var :=
+  mk_var { v_label_opt : option label; v_nat : nat }.
+Add Printing Constructor var.
 Unset Primitive Projections.
 
-Definition name_of_string (s : string) := mkname s 0.
-Coercion name_of_string : string >-> name.
+Definition var_of_string (s : string) := mk_var (Some s) 0.
+Coercion var_of_string : string >-> var.
 
-Declare Scope name_scope.
-Delimit Scope name_scope with name.
-Bind Scope name_scope with name.
-Arguments mkname n%name_scope i : rename.
+Declare Scope var_scope.
+Delimit Scope var_scope with var.
+Bind Scope var_scope with var.
+Arguments mk_var lo%var_scope n : rename.
 
 String Notation string string_of_list_byte list_byte_of_string
-  : name_scope.
+  : var_scope.
 Notation "s ₍₁₎" :=
-  (mkname s 1) (at level 9, format "s '₍₁₎'")
-  : name_scope.
+  (mk_var (Some s) 1) (at level 9, format "s '₍₁₎'")
+  : var_scope.
 Notation "s ₍₂₎" :=
-  (mkname s 2) (at level 9, format "s '₍₂₎'")
-  : name_scope.
+  (mk_var (Some s) 2) (at level 9, format "s '₍₂₎'")
+  : var_scope.
 Notation "s ₍₃₎" :=
-  (mkname s 3) (at level 9, format "s '₍₃₎'")
-  : name_scope.
+  (mk_var (Some s) 3) (at level 9, format "s '₍₃₎'")
+  : var_scope.
 Notation "s ₍₄₎" :=
-  (mkname s 4) (at level 9, format "s '₍₄₎'")
-  : name_scope.
+  (mk_var (Some s) 4) (at level 9, format "s '₍₄₎'")
+  : var_scope.
 Notation "s ₍₅₎" :=
-  (mkname s 5) (at level 9, format "s '₍₅₎'")
-  : name_scope.
+  (mk_var (Some s) 5) (at level 9, format "s '₍₅₎'")
+  : var_scope.
 Notation "s ₍₆₎" :=
-  (mkname s 6) (at level 9, format "s '₍₆₎'")
-  : name_scope.
+  (mk_var (Some s) 6) (at level 9, format "s '₍₆₎'")
+  : var_scope.
 Notation "s ₍₇₎" :=
-  (mkname s 7) (at level 9, format "s '₍₇₎'")
-  : name_scope.
+  (mk_var (Some s) 7) (at level 9, format "s '₍₇₎'")
+  : var_scope.
 Notation "s ₍₈₎" :=
-  (mkname s 8) (at level 9, format "s '₍₈₎'")
-  : name_scope.
+  (mk_var (Some s) 8) (at level 9, format "s '₍₈₎'")
+  : var_scope.
 Notation "s ₍₉₎" :=
-  (mkname s 9) (at level 9, format "s '₍₉₎'")
-  : name_scope.
-
-Definition name_dec (n1 n2 : name) : {n1 = n2} + {n1 <> n2}.
-  decide equality.
-  - apply index_dec.
-  - apply label_dec.
-Defined.
-
-Definition name_eqb n1 n2 : bool :=
-  match name_dec n1 n2 with
-  | left _ => true
-  | right _ => false
-  end.
-
-Definition shift_name n1 n2 :=
-  if label_dec (n_label n1) (n_label n2) then
-    if le_gt_dec (n_index n1) (n_index n2) then
-      (mkname (n_label n2) (S (n_index n2)))
-    else n2
-  else n2.
-Arguments shift_name n1 n2 : simpl nomatch.
-
-Definition unshift_name n1 n2 :=
-  if label_dec (n_label n1) (n_label n2) then
-    if Compare_dec.le_gt_dec (n_index n2) (n_index n1) then n2
-    else mkname (n_label n2) (pred (n_index n2))
-  else n2.
-Arguments unshift_name n1 n2 : simpl nomatch.
-
-(* Levels are nats *)
-
-Definition level := nat.
-
-Definition level_dec := Nat.eq_dec.
-
-Definition level_eqb l1 l2 : bool :=
-  match level_dec l1 l2 with
-  | left _ => true
-  | right _ => false
-  end.
-
-Definition shift_level (l1 l2 : level) : level :=
-  if le_gt_dec l1 l2 then S l2
-  else l2.
-Arguments shift_level l1 l2 : simpl nomatch.
-
-Definition unshift_level (l1 l2 : level) : level :=
-  match Compare_dec.le_gt_dec l2 l1 with
-  | left le => l2
-  | right le => pred l2
-  end.
-Arguments unshift_level l1 l2 : simpl nomatch.
-
-(* Variables are either free names or bound levels *)
-
-Inductive var :=
-| free (n : name)
-| bound (l : level).
+  (mk_var s 9) (at level 9, format "s '₍₉₎'")
+  : var_scope.
 
 Definition var_dec (v1 v2 : var) : {v1 = v2} + {v1 <> v2}.
   decide equality.
-  - apply name_dec.
-  - apply level_dec.
+  - apply Nat.eq_dec.
+  - apply label_opt_dec.
 Defined.
 
 Definition var_eqb v1 v2 : bool :=
@@ -121,222 +59,15 @@ Definition var_eqb v1 v2 : bool :=
   | right _ => false
   end.
 
-Definition var_opt_dec (vo1 vo2 : option var)
-  : {vo1 = vo2} + {vo1 <> vo2}.
-  decide equality.
-  apply var_dec.
-Defined.
-
-Definition var_opt_eqb vo1 vo2 : bool :=
-  match var_opt_dec vo1 vo2 with
-  | left _ => true
-  | right _ => false
-  end.
-
-Definition var_opt_var_dec (vo1 : option var) (v2 : var)
-  : {vo1 = Some v2} + {vo1 <> Some v2}.
-  decide equality.
-  apply var_dec.
-Defined.
-
-Definition var_opt_var_eqb vo1 v2 : bool :=
-  match var_opt_var_dec vo1 v2 with
-  | left _ => true
-  | right _ => false
-  end.
-
-Definition v_label_opt vo :=
-  match vo with
-  | bound _ => None
-  | free n => Some (n_label n)
-  end.
-
-Definition v_nat v :=
-  match v with
-  | bound l => l
-  | free n => n_index n
-  end.
-
-Definition mk_var so n :=
-  match so with
-  | None => bound n
-  | Some s => free (mkname s n)
-  end.
-
-Definition succ_var v :=
-  (mk_var (v_label_opt v) (S (v_nat v))).
-Arguments succ_var v /.
-Arguments succ_var v : simpl never.
-
-Definition pred_var v :=
-  (mk_var (v_label_opt v) (pred (v_nat v))).
-Arguments pred_var v /.
-Arguments pred_var v : simpl never.
-
-(* Utilities for manipulating
-   [option^n var -> option^m var] *)
-
-Fixpoint options N A :=
-  match N with
-  | 0 => A
-  | S N => option (options N A)
-  end.
-
-Definition var_op N M := options N var -> options M var.
-
-Declare Scope var_op_scope.
-Delimit Scope var_op_scope with var_op.
-Bind Scope var_op_scope with var_op.
-
-Definition var_op_id {N} : var_op N N :=
-  (fun v => v).
-Arguments var_op_id {N} v /.
-
-Notation " 1 " := var_op_id : var_op_scope.
-
-Definition var_op_compose {N M O} :
-  var_op M O -> var_op N M -> var_op N O :=
-  fun op1 op2 => fun v => op1 (op2 v).
-Arguments var_op_compose {N M O} op1 op2 v /.
-
-Notation "op1 @ op2" := (var_op_compose op1 op2)
-    (at level 60, right associativity)
-  : var_op_scope.
-
-Lemma var_op_left_identity {N M} :
-  forall (op : var_op N M),
-    (1 @ op = op)%var_op.
-Proof. reflexivity. Qed.
-
-Lemma var_op_right_identity {N M} :
-  forall (op : var_op N M),
-    (op @ 1 = op)%var_op.
-Proof. reflexivity. Qed.
-
-Lemma var_op_associative {N M O P} :
-  forall (op1 : var_op O P)
-         (op2 : var_op M O)
-         (op3 : var_op N M),
-    (op1 @ (op2 @ op3) = (op1 @ op2) @ op3)%var_op.
-Proof. reflexivity. Qed.
-
-Definition lift_var_op {N M} :
-  var_op N M -> var_op (S N) (S M) :=
-  fun op vo =>
-      match vo with
-      | None => None
-      | Some v => Some (op v)
-      end.
-
-Definition eq_var_op {N M} : relation (var_op N M) :=
-  @pointwise_relation (options N var) (options M var) eq.
-
-Notation "f =v= g" :=
-  (eq_var_op (f)%var_op (g)%var_op) (at level 70).
-
-Instance eq_var_op_equiv {N M} :
-  Equivalence (@eq_var_op N M).
-Proof.
-  apply @Build_Equivalence; try easy.
-  intros op1 op2 op3 Heq1 Heq2 v.
-  rewrite Heq1, Heq2; easy.
-Qed.
-
-Add Parametric Morphism {N M O} : (@var_op_compose N M O)
-    with signature eq_var_op ==> eq_var_op ==> eq_var_op
-      as var_op_compose_mor.
-  intros * Heq1 * Heq2 v; unfold var_op_compose.
-  rewrite Heq1, Heq2; easy.
-Qed.
-
-Add Parametric Morphism {N M} : (@lift_var_op N M)
-    with signature eq_var_op ==> eq_var_op
-      as lift_var_op_mor.
-  intros * Heq1 [v|]; unfold lift_var_op;
-    try rewrite Heq1; easy.
-Qed.
-
-(* Core operations on vars *)
-
-Definition shift_var v1 v2 :=
-  match v1, v2 with
-  | bound l1, bound l2 => bound (shift_level l1 l2)
-  | bound l, free n => free n
-  | free n, bound l => bound l
-  | free n1, free n2 => free (shift_name n1 n2)
-  end.
-Arguments shift_var !v1 !v2.
-
-Definition unshift_var v1 v2 :=
-  match v1, v2 with
-  | bound l1, bound l2 => bound (unshift_level l1 l2)
-  | free n, bound l => bound l
-  | bound l, free n => free n
-  | free n1, free n2 => free (unshift_name n1 n2)
-  end.
-Arguments unshift_var !v1 !v2.
-
-Definition pop_var v1 : var_op 1 0 :=
-  fun vo2 =>
-    match vo2 with
-    | None => v1
-    | Some v2 => shift_var v1 v2
-    end.
-Arguments pop_var v1 vo2 : simpl never.
-
-Definition push_var v1 : var_op 0 1 :=
-  fun v2 =>
-    if var_eqb v1 v2 then None
-    else Some (unshift_var v1 v2).
-Arguments push_var v1 v2 : simpl never.
-
-Definition swap_var : var_op 2 2 :=
-  fun voo =>
-    match voo with
-    | None => Some None
-    | Some None => None
-    | Some (Some v) => Some (Some v)
-    end.
-Arguments swap_var voo : simpl nomatch.
-
-(* [v1] is "shifting" [v2] another if [shift_var v1 v2 <> v2] *)
-
-Definition is_shifting_var v1 v2 :=
-  andb
-    (label_opt_eqb (v_label_opt v1) (v_label_opt v2))
-    (leb (v_nat v1) (v_nat v2)).
-
-Definition shifts_var v1 v2 :=
-  if is_shifting_var v1 v2 then sUnit
-  else sEmpty.
-
-(* [v1] is "unshifting" [v2] another if [unshift_var v1 v2 <> v2] *)
-
-Definition is_unshifting_var v1 v2 :=
-  andb
-    (label_opt_eqb (v_label_opt v1) (v_label_opt v2))
-    (Nat.ltb (v_nat v1) (v_nat v2)).
-
-Definition unshifts_var v1 v2 :=
-  if is_unshifting_var v1 v2 then sUnit
-  else sEmpty.
-
 (* Total ordering on vars *)
 
 Definition is_less_equal_var v1 v2 :=
   orb
     (is_less_than_label_opt (v_label_opt v1) (v_label_opt v2))
-    (is_shifting_var v1 v2).
+    (andb
+       (label_opt_eqb (v_label_opt v1) (v_label_opt v2))
+       (leb (v_nat v1) (v_nat v2))).
 
 Definition less_equal_var v1 v2 :=
   if is_less_equal_var v1 v2 then sUnit
-  else sEmpty.
-
-Definition is_less_than_var v1 v2 :=
-  orb
-    (is_less_than_label_opt (v_label_opt v1) (v_label_opt v2))
-    (is_unshifting_var v1 v2).
-
-Definition less_than_var v1 v2 :=
-  if is_less_than_var v1 v2 then sUnit
   else sEmpty.
